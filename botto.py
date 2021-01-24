4fa6db17df1fc64d577a3e0681b563169d3bf471
Aaron Jimenez <roceaaan@gmail.com>
2021-01-23 21:54:51 -0800

    Revert

--- botto.py
+++ botto.py
@@ -1,17 +1,16 @@
 import os
 import discord
 from discord.ext import commands
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 import CustomBotClass
 
-# load_dotenv()
+load_dotenv()
 
-# TOKEN = os.getenv('DISCORD_TOKEN')
-# GUILD_NAME= os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_NAME= os.getenv('DISCORD_GUILD')
 
 
-TOKEN = "ODAyMzg1NTY3ODc4MjE3NzM4.YAudyg.yibfMuCO_JdBJ_pD2ZH2OtKfUsw"
 client = CustomBotClass.DiscordBottoClient()
 
 client.run(TOKEN)
